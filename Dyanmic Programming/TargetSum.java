a0ab33ec67aa02a890521143b93a0d2c361a6758
Ajay-sys2-tech <ajaycu23@gmail.com>
2022-02-15 00:58:29 +0530

    target sum tabulation

--- Dyanmic Programming/TargetSum.java
+++ Dyanmic Programming/TargetSum.java
@@ -12,23 +12,41 @@ class TargetSum{
     }
 
 
-    public static int findTargetSumWays(int[] nums, int target) { 
+    public int findTargetSumWays(int[] nums, int target) {
+        
         
         int n = nums.length;
-       
+        
+        int sum = 0;
+        int zero = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0)zero++;
+            sum += nums[i];
+        }
+        
+        if(target > sum || target < -sum) return 0;
         count = 0;
-        recursive(nums, target, 0, 0);
-
+        
+        // recursive(nums, target, 0, 0);
         HashMap<String, Integer> memo = new HashMap<>();
-        return count;
+        
+        // return memorization(0, target, 0, nums, memo);
+        if((sum + target) % 2 == 0){
+            // if(zero == 0) return tabulation(nums, target, sum); 
+           // else
+               // if(zero > 1)
+               //     return (int)Math.pow(2, zero) *
+               //     tabulation(nums, target, sum);  
+               return tabulation(nums, target, sum);  
+        }
+       
+        
+        return 0;
+        
+        
         }
     
-
-
-    public static int count = 0;
-    public static int[][] dp;
-
-        public static int memorization(int i, int target, int sum, int[] nums,  HashMap<String, Integer> memo){
+    public static int memorization(int i, int target, int sum, int[] nums,  HashMap<String, Integer> memo){
             
 
             if(i == nums.length){
@@ -47,17 +65,44 @@ class TargetSum{
             return memo.get(key);
 
         }
-
-    public static void recursive(int[] nums, int target, int i, int sum){
+    
+    public int count = 0;
+    public void recursive(int[] nums, int target, int i, int sum){
         if(i == nums.length){
             if(sum == target){
                 count += 1;
             }
-            
+            return;
         }
-       
-        recursive(nums, target, i+1, sum + nums[i]); 
-        recursive(nums, target, i+1, sum - nums[i]);
+        
+        
+             recursive(nums, target, i+1, sum + nums[i]); 
+             recursive(nums, target, i+1, sum - nums[i]);
 
+    }
+    
+    
+    public static  int tabulation(int[] nums, int diff, int sum){
+       
+        int n = nums.length;
+        
+        sum = (sum + diff) / 2;
+        
+        int[][] dp = new int[n+1][sum + 1];
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
         }
+        
+        
+        return dp[n][sum];
+    }
 }
